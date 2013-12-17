f0e9fff8d45536aba30ce1dc8b72c4fa346e8f6c
Patrick Head <patrickhead@gmail.com>
2013-12-17 03:47:46 -0500

    More fix ups for GNU package compliance.

--- tools/autounfuck.sh
+++ tools/autounfuck.sh
@@ -4,8 +4,8 @@ find . -name 'Makefile.in' -exec rm {} \;
 rm -rf autom4te.cache
 rm -f config.h.in
 rm -f config.h.in~
-rm -f config/*
+rm -rf config
 rm -f aclocal.m4
-rm -f m4/*
+rm -rf m4
 rm -f configure
 rm -f egg-*.tar.gz
